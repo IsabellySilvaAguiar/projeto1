9ed0858f00df6e08b32c6ca9fd47bb209513db4d
IsabellySilvaAguiar <142235427+IsabellySilvaAguiar@users.noreply.github.com>
2023-09-11 08:01:04 -0400

    visita.js

---
+++ visita.js
@@ -0,0 +1,36 @@
+let currentImageIndex = 0;
+const images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img3.jpg','img4.jpg','img5.jpg','img6.jpg','img7.jpg','img8.jpg','img9.jpg','img10.jpg' ];
+
+
+function openModal(imageSrc) 
+{
+    const modal = document.getElementById('myModal');
+    const modalImage = document.getElementById('modalImage');
+
+    currentImageIndex = images.indexOf(imageSrc);
+    modal.style.display = 'block';
+    modalImage.src = imageSrc;
+}
+
+function closeModal() 
+{
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+    }
+
+function nextImage() 
+{
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        const modalImage = document.getElementById('modalImage');
+        modalImage.src = images[currentImageIndex];
+}
+
+const modal = document.getElementById('myModal');
+modal.addEventListener
+('click', function(event) 
+{
+    if (event.target === modal) {
+        closeModal();
+    }
+}
+);
